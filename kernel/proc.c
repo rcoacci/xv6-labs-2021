ed1f6131abbcf6c4c8ad59cde8d778ca0c46cfcc
Rodrigo Coacci <rcoacci@gmail.com>
2022-07-14 16:00:45 -0300

    Implementa sys_trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->tracemask = 0;
 }
 
 // Create a user page table for a given process,
@@ -304,6 +305,9 @@ fork(void)
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
+
+  // Copy trace mask
+  np->tracemask = p->tracemask;
 
   release(&np->lock);
 
